d30ae4fb9ae2813c65dd02199f73e7ca2f7486aa
Артур Мавлидов <arturmavlidov@192.168.1.3>
2025-06-18 23:38:17 +0300

    Add parser

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,5 +1,7 @@
 import { Command } from "commander";
 
+import { parser } from "./parser.js";
+
 export const gendiff = () => {
   const program = new Command();
 
@@ -8,7 +10,10 @@ export const gendiff = () => {
     .description("Compares two configuration files and shows a difference.")
     .arguments("filepath1 filepath2")
     .option("-V, --version", "output the version number")
-    .option("-f, --format [type]", "output format");
+    .option("-f, --format [type]", "output format")
+    .action(() => {
+      console.log(parser(program.args[0]));
+    });
 
   program.parse();
 };
---
+++ src/parser.js
@@ -0,0 +1,7 @@
+import { readFileSync } from "fs";
+
+export const parser = (filepath) => {
+  const file = readFileSync(filepath, { encoding: "utf8" });
+
+  return JSON.parse(file);
+};
