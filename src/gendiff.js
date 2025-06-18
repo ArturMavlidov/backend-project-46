43eabf84ac32bba6dd1142195937617278e2f00a
Артур Мавлидов <arturmavlidov@192.168.1.3>
2025-06-18 23:23:20 +0300

    Update gendiff.js options

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,7 +5,10 @@ export const gendiff = () => {
 
   program
     .name("gendiff")
-    .description("  Compares two configuration files and shows a difference.");
+    .description("Compares two configuration files and shows a difference.")
+    .arguments("filepath1 filepath2")
+    .option("-V, --version", "output the version number")
+    .option("-f, --format [type]", "output format");
 
   program.parse();
 };
