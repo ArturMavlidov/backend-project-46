b35b99c99c1c1bdb41515a6b5d9310bf28a5f756
Артур Мавлидов <arturmavlidov@192.168.1.3>
2025-06-20 23:08:35 +0300

    Add support for yaml file comparison

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,27 @@
-import { compareFiles } from '../src/gendiff';
-import { getFixturePath } from '../src/helpers';
+import { compareFiles } from "../src/gendiff";
+import { getFixturePath } from "../src/helpers";
 
-test('gendiff', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-
-  expect(compareFiles(file1, file2)).toBe(`{
+describe("gendiff", () => {
+  const resultString = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`);
+}`;
+
+  test("gendiff json files", () => {
+    const file1 = getFixturePath("file1.json");
+    const file2 = getFixturePath("file2.json");
+
+    expect(compareFiles(file1, file2)).toBe(resultString);
+  });
+
+  test("gendiff yml files", () => {
+    const file1 = getFixturePath("file1.yml");
+    const file2 = getFixturePath("file2.yaml");
+
+    expect(compareFiles(file1, file2)).toBe(resultString);
+  });
 });
--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,18 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
+import jsToYaml from "js-yaml";
+import path from "path";
 
 export const parser = (filepath) => {
-  const file = readFileSync(filepath, { encoding: 'utf8' });
+  const file = readFileSync(filepath, { encoding: "utf8" });
+  const fileExtension = path.extname(filepath);
+
+  const isYaml = fileExtension === ".yml" || fileExtension === ".yaml";
+
+  if (isYaml) {
+    const convertedYamlToJson = jsToYaml.load(file);
+
+    return convertedYamlToJson;
+  }
 
   return JSON.parse(file);
 };
