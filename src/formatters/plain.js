29faf6553040481ca80d464bdf01639e40deb711
Артур Мавлидов <arturmavlidov@192.168.1.6>
2025-07-03 18:20:40 +0300

    Fix plain formatter: trim result

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,51 +1,54 @@
-import { isObject } from '../helpers/index.js';
+import { isObject } from "../helpers/index.js";
 
 const stringifyValue = (value) => {
   if (isObject(value)) {
-    return '[complex value]';
+    return "[complex value]";
   }
 
-  if (typeof value !== 'string') {
+  if (typeof value !== "string") {
     return value;
   }
 
   return `'${value}'`;
 };
 
-export const plain = (data) => data.reduce((acc, val) => {
-  const {
-    operation, value, oldValue, ignoreInPlain,
-  } = val;
+export const plain = (data) => {
+  const iter = (data) => {
+    return data.reduce((acc, val) => {
+      const { operation, value, oldValue, ignoreInPlain } = val;
 
-  if (ignoreInPlain) {
-    return acc;
-  }
+      if (ignoreInPlain) {
+        return acc;
+      }
 
-  if (Array.isArray(value)) {
-    return `${acc.trim()}${plain(value)}`;
-  }
+      if (Array.isArray(value)) {
+        return `${acc}${iter(value)}`;
+      }
 
-  const stringifiedValue = stringifyValue(value);
-  const stringifiedOldValue = stringifyValue(oldValue);
+      const stringifiedValue = stringifyValue(value);
+      const stringifiedOldValue = stringifyValue(oldValue);
 
-  let result;
+      let result;
 
-  switch (operation) {
-    case 'removed':
-      result = 'removed';
-      break;
-    case 'added':
-      result = `added with value: ${stringifiedValue}`;
-      break;
-    case 'updated':
-      result = `updated. From ${stringifiedOldValue} to ${stringifiedValue}`;
-      break;
-    default:
-      console.error(`Unknown operation: ${operation}`);
-      break;
-  }
+      switch (operation) {
+        case "removed":
+          result = "removed";
+          break;
+        case "added":
+          result = `added with value: ${stringifiedValue}`;
+          break;
+        case "updated":
+          result = `updated. From ${stringifiedOldValue} to ${stringifiedValue}`;
+          break;
+        default:
+          console.error(`Unknown operation: ${operation}`);
+          break;
+      }
 
-  return `${acc}\nProperty '${val.property}' was ${result}`;
-}, '');
+      return `${acc}\nProperty '${val.property}' was ${result}`;
+    }, "");
+  };
 
+  return iter(data).trim();
+};
 export default plain;
