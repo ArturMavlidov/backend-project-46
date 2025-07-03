3259617c7a3edf06546259429acdc53bb0f0ebb6
Артур Мавлидов <arturmavlidov@192.168.1.6>
2025-07-03 18:24:24 +0300

    Fix linter errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,40 +13,41 @@ const stringifyValue = (value) => {
 };
 
 export const plain = (data) => {
-  const iter = (coll) =>
-    coll.reduce((acc, val) => {
-      const { operation, value, oldValue, ignoreInPlain } = val;
+  const iter = (coll) => coll.reduce((acc, val) => {
+    const {
+      operation, value, oldValue, ignoreInPlain,
+    } = val;
 
-      if (ignoreInPlain) {
-        return acc;
-      }
+    if (ignoreInPlain) {
+      return acc;
+    }
 
-      if (Array.isArray(value)) {
-        return `${acc}${iter(value)}`;
-      }
+    if (Array.isArray(value)) {
+      return `${acc}${iter(value)}`;
+    }
 
-      const stringifiedValue = stringifyValue(value);
-      const stringifiedOldValue = stringifyValue(oldValue);
+    const stringifiedValue = stringifyValue(value);
+    const stringifiedOldValue = stringifyValue(oldValue);
 
-      let result;
+    let result;
 
-      switch (operation) {
-        case "removed":
-          result = "removed";
-          break;
-        case "added":
-          result = `added with value: ${stringifiedValue}`;
-          break;
-        case "updated":
-          result = `updated. From ${stringifiedOldValue} to ${stringifiedValue}`;
-          break;
-        default:
-          console.error(`Unknown operation: ${operation}`);
-          break;
-      }
+    switch (operation) {
+      case 'removed':
+        result = 'removed';
+        break;
+      case 'added':
+        result = `added with value: ${stringifiedValue}`;
+        break;
+      case 'updated':
+        result = `updated. From ${stringifiedOldValue} to ${stringifiedValue}`;
+        break;
+      default:
+        console.error(`Unknown operation: ${operation}`);
+        break;
+    }
 
-      return `${acc}\nProperty '${val.property}' was ${result}`;
-    }, "");
+    return `${acc}\nProperty '${val.property}' was ${result}`;
+  }, '');
 
   return iter(data).trim();
 };
